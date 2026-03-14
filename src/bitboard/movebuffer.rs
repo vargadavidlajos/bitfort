6e9aa1bf9f331cc1de86cf175dc390fde9b9c1da
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2026-03-14 16:27:26 +0100

    implemented getters and setters for struct MoveBuffer in bitboard/movebuffer.rs

--- src/bitboard/movebuffer.rs
+++ src/bitboard/movebuffer.rs
@@ -14,4 +14,31 @@ impl MoveBuffer {
       count: 0
     };
   }
+
+  #[inline]
+  pub fn add(&mut self, bitmove: BitMove) {
+    self.buffer[self.count] = bitmove;
+    self.count += 1;
+  }
+  #[inline]
+  pub fn append(&mut self, other: &MoveBuffer) {
+    self.buffer[self.count..self.count + other.count()].copy_from_slice(other.contents());
+    self.count += other.count();
+  }
+  #[inline(always)]
+  pub fn clear(&mut self) {
+    self.count = 0;
+  }
+  #[inline(always)]
+  pub fn count(&self) -> usize{
+    return self.count;
+  }
+  #[inline(always)]
+  pub fn get(&self, idx: usize) -> &BitMove {
+    return &self.buffer[idx];
+  }
+  #[inline(always)]
+  pub fn contents(&self) -> &[BitMove] {
+    return &self.buffer[0..self.count];
+  } 
 }
