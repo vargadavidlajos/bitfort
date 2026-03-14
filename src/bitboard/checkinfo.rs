db9e0db9eec4be6ebb52e4aab8e8ab79cef66156
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2026-03-14 15:48:03 +0100

    implemented constructor for struct CheckInfo in bitboard/checkinfo.rs

--- src/bitboard/checkinfo.rs
+++ src/bitboard/checkinfo.rs
@@ -3,3 +3,13 @@ pub struct CheckInfo {
   pub check_count: u8,
   pub move_mask: u64
 }
+
+impl CheckInfo {
+
+  pub fn new() -> Self {
+    return Self {
+      check_count: 0,
+      move_mask: 0xFFFF_FFFF_FFFF_FFFF
+    }
+  }
+}
