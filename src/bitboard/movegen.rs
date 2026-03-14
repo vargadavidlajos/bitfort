51cb72576bb5a66cfc78894e2f37afb3a9c13333
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2026-03-14 18:38:47 +0100

    implemented combined move generation method collect_moves_single_check in bitboard/movegen.rs

--- src/bitboard/movegen.rs
+++ src/bitboard/movegen.rs
@@ -8,6 +8,7 @@ pub mod kings;
 use super::board::Board;
 use super::movebuffer::MoveBuffer;
 use super::bitmove::BitMove;
+use super::checkinfo::CheckInfo;
 use super::utils::*;
 
 impl Board {
@@ -24,6 +25,19 @@ impl Board {
     self.add_queen_moves(buffer, temp_buffer, Self::NO_FILTER);
     self.add_king_moves(buffer, temp_buffer, safe_squares);
     self.add_king_castles(buffer, safe_squares);
+
+    buffer.append(temp_buffer);
+    temp_buffer.clear();
+  }
+  pub fn collect_moves_single_check(&self, buffer: &mut MoveBuffer, temp_buffer: &mut MoveBuffer, check_info: &CheckInfo) {
+    let safe_squares = self.get_safe_king_squares();
+
+    self.add_pawn_moves(buffer, temp_buffer, check_info.move_mask);
+    self.add_knight_moves(buffer, temp_buffer, check_info.move_mask);
+    self.add_bishop_moves(buffer, temp_buffer, check_info.move_mask);
+    self.add_rook_moves(buffer, temp_buffer, check_info.move_mask);
+    self.add_queen_moves(buffer, temp_buffer, check_info.move_mask);
+    self.add_king_moves(buffer, temp_buffer, safe_squares);
 
     buffer.append(temp_buffer);
     temp_buffer.clear();
