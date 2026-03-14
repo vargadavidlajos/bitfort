866bb517d9dad548a0595764acb8edc84403218b
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2026-03-14 16:58:41 +0100

    implemented move generation for rooks in bitboard/movegen/rooks.rs

---
+++ src/bitboard/movegen/rooks.rs
@@ -0,0 +1,58 @@
+use super::*;
+
+impl Board {
+
+  pub fn add_rook_moves(&self, capture_buffer: &mut MoveBuffer, quiet_buffer: &mut MoveBuffer, move_mask: u64) {
+    let piece_index = 3 + self.side_to_move * 6;
+    let mut rooks = self.bitboards[piece_index as usize];
+    let empty = !self.occupancy[2];
+    let opponents = self.occupancy[1 - self.side_to_move as usize];
+    while rooks != 0 {
+      let from_sq = pop_lsb(&mut rooks);
+      let raw_move_map = self.get_pseudo_rook_moves(from_sq) & move_mask;
+      let move_map = self.get_pin_masked_moves(raw_move_map, from_sq);
+
+      let mut quiet_map = move_map & empty;
+      let mut capture_map = move_map & opponents;
+
+      while quiet_map != 0 {
+        let to_sq = pop_lsb(&mut quiet_map);
+        quiet_buffer.add(BitMove::quiet(
+          from_sq,
+          to_sq,
+          false,
+          1
+        ));
+      }
+      while capture_map != 0 {
+        let to_sq = pop_lsb(&mut capture_map);
+        capture_buffer.add(BitMove::capture(
+          from_sq,
+          to_sq,
+          false,
+          1
+        ));
+      }
+    }
+  }
+  pub fn add_rook_captures(&self, buffer: &mut MoveBuffer, move_mask: u64) {
+    let offset = 6 * self.side_to_move as usize;
+    let mut rooks: u64 = self.bitboards[3 + offset];
+    let opponents = self.occupancy[1 - self.side_to_move as usize];
+    while rooks != 0 {
+      let next_sq: u32 = pop_lsb(&mut rooks);
+      let mut attacks: u64 = self.get_pseudo_rook_moves(next_sq) & opponents & move_mask;
+      attacks = self.get_pin_masked_moves(attacks, next_sq);
+
+      while attacks != 0 {
+        let to_sq = pop_lsb(&mut attacks);
+        buffer.add(BitMove::capture(
+          next_sq,
+          to_sq,
+          false,
+          1
+        ));
+      }
+    }
+  }
+}
