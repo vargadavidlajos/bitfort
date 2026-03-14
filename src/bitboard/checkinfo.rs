cebccfc3133520cd1ec8ad390500a45c9ca45116
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2026-03-14 15:50:51 +0100

    implemented method add_checker for struct CheckInfo in bitboard/checkinfo.rs

--- src/bitboard/checkinfo.rs
+++ src/bitboard/checkinfo.rs
@@ -12,4 +12,10 @@ impl CheckInfo {
       move_mask: 0xFFFF_FFFF_FFFF_FFFF
     }
   }
+
+  #[inline(always)]
+  pub fn add_checker(&mut self, move_mask: u64) {
+    self.move_mask &= move_mask;
+    self.check_count += 1;
+  }
 }
