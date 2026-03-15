a3d177eae90bd78e5baa585808b031c6de2e6304
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2026-03-15 17:42:02 +0100

    defined struct Engine in search.rs

--- src/search.rs
+++ src/search.rs
@@ -1,1 +1,14 @@
 pub mod searchcontext;
+
+use crate::bitboard::movebuffer::MoveBuffer;
+
+use searchcontext::SearchContext;
+
+
+pub const MAX_DEPTH: usize = 21;
+
+pub struct Engine {
+  search_buffers: [MoveBuffer; MAX_DEPTH],
+  temp_buffer: MoveBuffer,
+  search_depth: u8
+}
