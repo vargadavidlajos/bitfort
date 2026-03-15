9043d169cab5fbe555f045d5500a0aad415bd035
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2026-03-15 17:13:56 +0100

    implemented evaluation based on material in bitboard/evaluation/material.rs

--- src/bitboard/evaluation.rs
+++ src/bitboard/evaluation.rs
@@ -2,3 +2,6 @@ pub mod luts;
 pub mod material;
 pub mod pst;
 pub mod pawnstructure;
+
+use super::board::Board;
+use luts::*;
---
+++ src/bitboard/evaluation/material.rs
@@ -0,0 +1,13 @@
+use super::*;
+
+impl Board {
+  #[inline(always)]
+  fn eval_by_material(&self) -> i32 {
+    let eval = (self.bitboards[0].count_ones() as i32 - self.bitboards[6].count_ones() as i32) * PIECE_VALUE[0]
+                  + (self.bitboards[1].count_ones() as i32 - self.bitboards[7].count_ones() as i32) * PIECE_VALUE[1]
+                  + (self.bitboards[2].count_ones() as i32 - self.bitboards[8].count_ones() as i32) * PIECE_VALUE[2]
+                  + (self.bitboards[3].count_ones() as i32 - self.bitboards[9].count_ones() as i32) * PIECE_VALUE[3]
+                  + (self.bitboards[4].count_ones() as i32 - self.bitboards[10].count_ones() as i32) * PIECE_VALUE[4];
+    return eval;
+  }
+}
