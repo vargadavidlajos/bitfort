a49fc83bdea187244f0165c24d38a8fb40cba791
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2026-03-15 10:46:36 +0100

    added file and module structure for move reverting logic

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -8,3 +8,4 @@ pub mod movegen;
 pub mod checkinfo;
 pub mod movebuffer;
 pub mod makemove;
+pub mod unmakemove;
---
+++ src/bitboard/unmakemove.rs
@@ -0,0 +1,4 @@
+pub mod quiets;
+pub mod captures;
+pub mod castles;
+pub mod enpassants;
