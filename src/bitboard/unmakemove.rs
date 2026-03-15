5b71af4d93da212eab142b9889cea4eb15a8b848
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2026-03-15 11:35:49 +0100

    added skeleton for method unmake_move in bitboard/unmakemove.rs

--- src/bitboard/unmakemove.rs
+++ src/bitboard/unmakemove.rs
@@ -2,3 +2,37 @@ pub mod quiets;
 pub mod captures;
 pub mod castles;
 pub mod enpassants;
+
+use super::board::Board;
+use super::unmakeinfo::UnmakeInfo;
+use super::bitmove::BitMove;
+
+impl Board {
+  #[inline]
+  pub fn unmake_move(&mut self, played_move: &BitMove, undo_info: &UnmakeInfo) {
+    let move_type: u8 = played_move.move_type();
+
+    match move_type {
+      BitMove::QUIET => {
+        
+      }
+      BitMove::CAPTURE => {
+        
+      }
+      BitMove::CASTLE => {
+        
+      }
+      BitMove::EN_PASSANT => {
+        
+      }
+      _ => { panic!("Tried to revert move of invalid type!"); }
+    }
+
+    self.castling_rights = undo_info.castling_rights();
+    self.en_passant_square = undo_info.en_passant_square();
+
+    self.side_to_move = 1 - self.side_to_move;
+
+    self.occupancy[2] = self.occupancy[0] | self.occupancy[1];
+  }
+}
