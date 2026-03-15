84d921fb552e9186d6d764e332fbc01e6c74a5f2
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2026-03-15 18:48:52 +0100

    implemented method get_bulk_pseudo_bishop_moves in bitboard/attacks.rs

--- src/bitboard/attacks.rs
+++ src/bitboard/attacks.rs
@@ -240,6 +240,22 @@ impl Board {
 
     return moves;
   }
+
+  #[inline]
+  pub fn get_bulk_pseudo_bishop_moves(&self, mut bishops: u64) -> u64 {
+    let mut moves = 0u64;
+    let avoid_friendly = !self.occupancy[self.side_to_move as usize];
+
+    while bishops != 0 {
+      let bishop_sq = bishops.trailing_zeros();
+      bishops &= !(1 << bishop_sq);
+
+      let mask = self.get_pseudo_bishop_moves(bishop_sq) & avoid_friendly;
+      moves |= self.get_pin_masked_moves(mask, bishop_sq);
+    }
+
+    return moves;
+  }
 }
 #[inline(always)]
 pub fn get_raycast_from_square_in_direction(occupancy: u64, sq: usize, dir: usize) -> u64 {
