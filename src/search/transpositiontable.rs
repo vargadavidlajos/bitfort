f033c5b11050d7df626630ba53f219d01af1eec4
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2026-03-16 12:34:57 +0100

    implemented constructor for struct TranspositionTable in search/transpositiontable.rs

--- src/search/transpositiontable.rs
+++ src/search/transpositiontable.rs
@@ -9,3 +9,18 @@ pub struct TranspositionTable {
   pub calls: u64,
   pub hits: u64
 }
+
+impl TranspositionTable {
+
+  pub fn new(hash_length: u32) -> Self {
+    let capacity = 2u64.pow(hash_length) as usize;
+    let default_entry = TTEntry::null();
+    return Self {
+      table: vec![default_entry; capacity],
+      generation: 0,
+      hash_mask: !0u64 >> (64 - hash_length),
+      calls: 0,
+      hits: 0
+    };
+  }
+}
