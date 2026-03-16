9a75eb5916d15435bb0e850ae43afdb6ed0f6c77
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2026-03-16 12:34:19 +0100

    defined struct TranspositionTable in search/transpositiontable.rs

--- src/search/transpositiontable.rs
+++ src/search/transpositiontable.rs
@@ -1,1 +1,11 @@
 pub mod transpositionentry;
+
+use transpositionentry::TTEntry;
+
+pub struct TranspositionTable {
+  table: Vec<TTEntry>,
+  generation: u8,
+  hash_mask: u64,
+  pub calls: u64,
+  pub hits: u64
+}
