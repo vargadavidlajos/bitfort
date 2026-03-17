c20266c1043245d6f658f39414bdac1415663dbb
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2026-03-17 13:28:35 +0100

    added fields killer1 and killer2 to struct SearchContext in search/searchcontext.rs

--- src/search/searchcontext.rs
+++ src/search/searchcontext.rs
@@ -1,5 +1,10 @@
+use crate::bitboard::bitmove::BitMove;
+
+use super::MAX_DEPTH;
 
 pub struct SearchContext {
+  pub killer1: [Option<BitMove>; MAX_DEPTH],
+  pub killer2: [Option<BitMove>; MAX_DEPTH],
   pub nodes: u64,
   pub ply: usize
 }
@@ -7,6 +12,8 @@ pub struct SearchContext {
 impl SearchContext {
   pub fn new() -> Self {
     return Self {
+      killer1: [None; MAX_DEPTH],
+      killer2: [None; MAX_DEPTH],
       nodes: 0,
       ply: 0
     }
